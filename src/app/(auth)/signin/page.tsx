7336892ce10ac0fb122885a94168d7507acde5d5
erik-ferreira <erikdesouzagf@gmail.com>
2023-04-30 00:50:44 -0300

    notes: differents files

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function SignIn() {
-  return <h1>Signin</h1>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <Link href="/app">Dashboard</Link>
+    </div>
+  );
 }
